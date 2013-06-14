2dfcb7bcc0ed64eab489e5fbe015daf1b1539231
Damien Dart <damiendart@pobox.com>
2013-06-14 20:16:57 +0100

    Mess with "finished-loading-stuff" animations.

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
-var spinner = new Spinner({color: "#ccc", corners: 1, hwaccel: true,
-    length: 0, lines: 12,radius: 15, width: 5}).spin();
+var spinner = new Spinner({className: "js-spinner", color: "#ccc", corners: 1,
+    hwaccel: true, length: 0, lines: 12,radius: 15, width: 5}).spin();
 document.getElementsByTagName("body").item(0).appendChild(spinner.el);
 $(window).load(function() {
-  $(".spinner").addClass("animated fadeOut");
-  setTimeout(function() { 
-    spinner.stop(); 
-    $(".site-image").show().addClass("animated fadeInDown");
-    $(".site-header").show().addClass("animated fadeInUp");
-    $(".site-navigation").show().addClass("animated fadeInUp");
-  }, 1000);
+  $(".spinner").addClass("js-fade-out");
+  setTimeout(function() {
+    spinner.stop();
+    $(".site-image").show().addClass("js-fade-in-from-top");
+    setTimeout(function() {
+      $(".site-header").show().addClass("js-fade-in-from-bottom");
+      setTimeout(function() {
+        $(".site-navigation").show().addClass("js-fade-in-from-bottom");
+      }, (window.innerWidth < 767) ? 250 : 0);
+    }, (window.innerWidth < 767) ? 250 : 0);
+  }, 500);
+  setTimeout(function() {
+    $(".site-image, .site-header, .site-navigation").addClass(
+        "js-no-animation");
+  }, 1500);
 });
