9ce9aef50dadecd905455945e5ff1d6dadc8ccf4
Damien Dart <damiendart@pobox.com>
2013-06-25 23:43:48 +0100

    Fiddle with The Folder of Crap markup and styles.

--- site/assets/_folder-of-crap.js
+++ site/assets/_folder-of-crap.js
@@ -1,5 +1,5 @@
-$(".content").addClass("tk-proxima-nova");
-$(".content__header").addClass("tk-league-gothic");
+$(".container").addClass("tk-proxima-nova");
+$(".page-header").addClass("tk-league-gothic");
 $("td:nth-child(2)").each(function() {
   $(this).text(moment($.trim($(this).text()), "DD-MMM-YYYY HH:mm").fromNow());
 });
