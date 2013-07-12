5205890e7806ea1997d02e8bb2b1554e34418d04
Damien Dart <damiendart@pobox.com>
2013-07-12 21:00:53 +0100

    Change how Typekit fonts are applied.
    Prevent the FOUC that sometimes occurs by having Typekit apply fonts
    instead of handling it ourselves.

--- site/assets/_folder-of-crap.js
+++ site/assets/_folder-of-crap.js
@@ -1,5 +1,3 @@
-$(".container").addClass("tk-proxima-nova");
-$(".page-header").addClass("tk-league-gothic");
 $("td:nth-child(2)").each(function() {
   $(this).text(moment($.trim($(this).text()), "DD-MMM-YYYY HH:mm").fromNow());
 });
