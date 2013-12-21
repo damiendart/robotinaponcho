1b2cb70c8a765f246249f6c2c7794091f399fa23
Damien Dart <damiendart@pobox.com>
2013-12-21 14:38:55 +0000

    Make "gitweb/build_gitweb.sh" a touch more robust.

--- gitweb/build_gitweb.sh
+++ gitweb/build_gitweb.sh
@@ -4,15 +4,18 @@
 
 # TODO: Clean up and make more robust.
 
+cleanup() {
+  rm -rf git-1.8.5.2
+}
+
+trap "cleanup" EXIT
 USAGE=`cat <<USAGE;
 USAGE: $(basename $0) [HTTP-DOC-FOLDER] [ADDITIONAL-BITS-FOLDER]
 WARNING: When entering directories, remove the trailing forward-slash.
 USAGE`
 [ "$#" -lt 2 ] && { echo "Not enough operands\n$USAGE" >&2; exit 2; }
-wget --progress=dot https://github.com/git/git/archive/v1.8.5.2.tar.gz
-tar -zxf v1.8.5.2.tar.gz
-cd git-1.8.5.2
-make GITWEB_PROJECTROOT="$1/git" \
+wget -O - https://github.com/git/git/archive/v1.8.5.2.tar.gz | tar -zx
+(cd git-1.8.5.2 && make GITWEB_PROJECTROOT="$1/git" \
     GITWEB_JS="/assets/gitweb.js" \
     GITWEB_CSS="/assets/gitweb.css" \
     GITWEB_SITENAME="GitHub Mirror" \
@@ -21,6 +24,5 @@ make GITWEB_PROJECTROOT="$1/git" \
     GITWEB_CONFIG="$PROTECTED_FOLDER/gitweb_config.perl" \
     bindir="/usr/local/bin" \
     gitwebdir="$1/git" \
-    gitweb install-gitweb
-mv $1/git/static/* $1/assets && rm -fr $1/git/static
-cd .. && rm -rf git-1.8.5.2 v1.8.5.2.tar.gz
+    gitweb install-gitweb &&
+    mv $1/git/static/* $1/assets && rm -fr $1/git/static)
