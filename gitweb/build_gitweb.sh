86346abefe51e5e18edb2eb93925b83a302e8c4a
Damien Dart <damiendart@pobox.com>
2013-12-24 21:14:48 +0000

    Fiddle around with "gitweb/build_gitweb.sh".
      - Add ability to specify the make command used.
      - Incorporate the "noindex" patch into the build script.
      - Remove trace command.
      - Update the usage string.
      - Update the "tar" command to be more explicit.

--- gitweb/build_gitweb.sh
+++ gitweb/build_gitweb.sh
@@ -4,18 +4,29 @@
 
 # TODO: Clean up and make more robust.
 
-set -x
 trap "rm -rf git-1.8.5.2" EXIT
 USAGE=`cat <<USAGE;
-USAGE: $(basename $0) [HTTP-DOC-FOLDER] [ADDITIONAL-BITS-FOLDER]
+USAGE: $(basename $0) HTDOCS-FOLDER PROTECTED-FOLDER [MAKE-COMMAND]
 USAGE`
 [ "$#" -lt 2 ] && { echo "Not enough operands\n$USAGE" >&2; exit 2; }
-wget -O - https://github.com/git/git/archive/v1.8.5.2.tar.gz | tar -zx
-(cd git-1.8.5.2 && make bindir="/usr/local/bin" \
+wget -O - https://github.com/git/git/archive/v1.8.5.2.tar.gz | tar -zxf -
+(cd git-1.8.5.2 && ${3:-make} bindir="/usr/local/bin" \
     GITWEB_CONFIG="${2%/}/gitweb_config.perl" \
     GITWEB_CSS="/assets/gitweb.css" GITWEB_FAVICON="/assets/git-favicon.png" \
     GITWEB_JS="/assets/gitweb.js" GITWEB_LOGO="/assets/git-logo.png" \
     GITWEB_PROJECTROOT="${1%/}/git" GITWEB_SITENAME="GitHub Mirror" \
     gitwebdir="${1%/}/git" gitweb install-gitweb &&
     mv ${1%/}/git/static/* ${1%/}/assets && rm -fr ${1%/}/git/static)
-patch ${1%/}/git/gitweb.cgi < gitweb/gitweb.patch
+patch ${1%/}/git/gitweb.cgi <<'PATCH'
+--- Original
++++ Modified
+@@ -4066,7 +4066,7 @@
+ <head>
+ <meta http-equiv="content-type" content="$content_type; charset=utf-8"/>
+ <meta name="generator" content="gitweb/$version git/$git_version$mod_perl_version"/>
+-<meta name="robots" content="index, nofollow"/>
++<meta name="robots" content="noindex, nofollow"/>
+ <title>$title</title>
+ EOF
+ 	# the stylesheet, favicon etc urls won't work correctly with path_info
+PATCH
