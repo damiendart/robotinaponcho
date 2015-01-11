e3e17d56959e4744255e7f002c410af9d33dc68e
Damien Dart <damiendart@pobox.com>
2015-01-11 11:38:11 +0000

    Move homepage JavaScript stuff into its own file.

---
+++ www/public/_index.js
@@ -0,0 +1,16 @@
+// Homepage JavaScript for Damien Dart's personal website.
+//
+// Copyright (C) 2013, 2014 Damien Dart, <damiendart@pobox.com>.
+// This file is distributed under the MIT licence. For more information,
+// please refer to the accompanying "LICENCE" file.
+
+function easterEgg() {
+if (window.location.hash == "#iloveyou") {
+  var links = document.getElementsByTagName("a");
+  for (var i = 0; i < links.length; i++) {
+    links[i].onmouseover = function() {
+      this.style.fontFamily = "\"Comic Sans MS\", \"Comic Sans\"";
+      this.style.lineHeight = "1";
+}; } } }
+window.onhashchange = easterEgg;
+easterEgg();
