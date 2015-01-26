4181d023db9ddd7e4621eaf63d2bba1d2357af85
Damien Dart <damiendart@pobox.com>
2015-01-26 20:37:30 +0000

    Update licensing bumf.

--- www/public/_index.js
+++ www/public/_index.js
@@ -1,6 +1,6 @@
 // Homepage JavaScript for Damien Dart's personal website.
 //
-// Copyright (C) 2013, 2014 Damien Dart, <damiendart@pobox.com>.
+// Copyright (C) 2013-2015 Damien Dart, <damiendart@pobox.com>.
 // This file is distributed under the MIT licence. For more information,
 // please refer to the accompanying "LICENCE" file.
 
