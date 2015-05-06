e5bd55fd728c49dec61dc6ef7cbf550dbb8989ff
Damien Dart <damiendart@pobox.com>
2015-05-06 11:51:38 +0100

    Update file comments.

--- www/public/crap/_index.pl
+++ www/public/crap/_index.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl -wT
 # Folder of Crap CGI script for Damien Dart's personal website.
 #
-# Copyright (C) 2013, 2014 Damien Dart, <damiendart@pobox.com>.
+# Copyright (C) 2013-2015 Damien Dart, <damiendart@pobox.com>.
 # This file is distributed under the MIT licence. For more information,
 # please refer to the accompanying "LICENCE" file.
 
