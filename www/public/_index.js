34cec64a647a1b91f1dc3146b061ce8dbd8d9df2
Damien Dart <damiendart@pobox.com>
2015-08-20 20:45:13 +0100

    Do some rearrangin'.

---
+++ www/public/_index.js
@@ -0,0 +1,20 @@
+// Homepage JavaScript for Damien Dart's personal website.
+//
+// Copyright (C) 2013-2015 Damien Dart, <damiendart@pobox.com>.
+// This file is distributed under the MIT licence. For more information,
+// please refer to the accompanying "LICENCE" file.
+
+$(".panel").each(function(i) {
+  if($(this).attr("data-image")) {
+    $(this).prepend("<img class=\"panel__js-image\" onLoad=\"this.style.opacity = 1\" src=\"" + 
+        $(this).data("image") + "\">");
+  } else if($(this).attr("data-code-example")) {
+    var panel = $(this);
+    $.get($(this).data("code-example"), function(data) {
+      panel.addClass("panel--js-code");
+      line_number = parseInt(panel.data("code-example").split("#")[1]);
+      code = data.split("\n").slice(line_number, line_number + 25).join("\n");
+      panel.prepend("<pre class=\"panel__js-code tk-source-code-pro\">" + code + "</pre>");
+    });
+  }
+});
