e3f45730bd9522100b4cc333d2e724b64684a84a
Damien Dart <damiendart@pobox.com>
2016-01-08 22:11:39 +0000

    Make date/time formats consistent across pages.

--- www/public/crap/_index.pl
+++ www/public/crap/_index.pl
@@ -21,7 +21,7 @@ closedir($directory);
 my @table_data;
 foreach (@directory_contents) {
   my %row = (filename => $_,
-      modification_date => strftime("%d-%m-%Y %R", localtime((stat($_))[9])),
+      modification_date => strftime("%F %T %z", localtime((stat($_))[9])),
       size => sprintf("%.1f KB", ((stat($_))[7] / 1024)),
       timestamp => (stat($_))[9], uri => URI->new($_));
   $row{size} =~ s/\.0//g;
