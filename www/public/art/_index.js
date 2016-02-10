7a0118e4f7ba44a1b06bc8564e8afe7edc521e15
Damien Dart <damiendart@pobox.com>
2016-02-10 20:47:46 +0000

    Fiddle with art portfolio loading stuff.

--- www/public/art/_index.js
+++ www/public/art/_index.js
@@ -1,4 +1,15 @@
+setTimeout(function() {
+  if (document.readyState != "complete") {
+    var loading_text = document.createElement("div");
+    loading_text.className = "js-loading";
+    loading_text.appendChild(document.createTextNode("Loading images"));
+    document.body.appendChild(loading_text);
+  }
+}, 750);
 window.onload = function() {
+  if (document.querySelector(".js-loading")) {
+    document.querySelector(".js-loading").className += " js-loading--hidden";
+  }
   var images = document.querySelectorAll(".image");
   for (var i = 0; i < images.length; i++) {
     images[i].className += " image--js-show";
