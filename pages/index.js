94d81124b1189f7a019644b8e99de05594a769f1
Damien Dart <damiendart@pobox.com>
2016-05-04 13:07:13 +0100

    Refactor "pages/index.js".

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,21 @@
-var images = document.querySelectorAll(".project-list__item__image");
+var images = document.querySelectorAll("[data-code-example]");
 for (var i = 0; i < images.length; i++) {
   (function() {
-    if (images[i].hasAttribute("data-code-example")) {
-      var request = new XMLHttpRequest();
-      request.open("GET", images[i].getAttribute("data-code-example"), true);
-      request.onreadystatechange = function(el) {
-        return function() {
-          if (request.readyState === XMLHttpRequest.DONE) {
-            var code_html = document.createElement("pre");
-            code_html.className = "project-list__item__image__js-code";
-            var line_number = parseInt(
-                el.getAttribute("data-code-example").split("#")[1]);
-            code_html.innerHTML = this.responseText.split(
-                "\n").slice(line_number, line_number + 25).join("\n");
-            el.insertBefore(code_html, el.firstChild);
-          }
+    var request = new XMLHttpRequest();
+    request.open("GET", images[i].getAttribute("data-code-example"), true);
+    request.onreadystatechange = function(el) {
+      return function() {
+        if (request.readyState === XMLHttpRequest.DONE) {
+          var code_html = document.createElement("pre");
+          code_html.className = "js-code-example";
+          var line_number = parseInt(
+              el.getAttribute("data-code-example").split("#")[1]);
+          code_html.innerHTML = this.responseText.split(
+              "\n").slice(line_number, line_number + 25).join("\n");
+          el.insertBefore(code_html, el.firstChild);
         }
-      }(images[i]);
-      request.send()
-    }
+      }
+    }(images[i]);
+    request.send()
   })();
 }
