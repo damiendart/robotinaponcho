1f5a05ae75cbd1d26d05c139a23b045269834e0c
Damien Dart <damiendart@pobox.com>
2016-08-25 14:30:42 +0100

    Change the homepage (yet again).
    Revert the homepage design to an earlier one, tweaking and updating a
    bunch of things along the way.

---
+++ index.js
@@ -0,0 +1,41 @@
+setTimeout(function() {
+  if (document.readyState != "complete") {
+    var document_body = document.querySelector("body");
+    var loading = document.createElement("div");
+    loading.className = "loading";
+    document_body.parentNode.insertBefore(loading, document_body.nextSibling);
+    for (var i = 0; i < 3; i++) {
+      loading.insertBefore(document.createElement("div"), loading.firstChild);
+    }
+  }
+}, 750);
+window.onload = function() {
+  var panels = document.querySelectorAll(".project-list__item");
+  for (var i = 0; i < panels.length; i++) {
+    panels[i].className += " project-list__item--js-show";
+  }
+  if (document.querySelector(".loading")) {
+    document.querySelector(".loading").className += " loading--js-fade-away";
+  }
+}
+var images = document.querySelectorAll("[data-code-example]");
+for (var i = 0; i < images.length; i++) {
+  (function() {
+    var request = new XMLHttpRequest();
+    request.open("GET", images[i].getAttribute("data-code-example"), true);
+    request.onreadystatechange = function(el) {
+      return function() {
+        if (request.readyState === XMLHttpRequest.DONE) {
+          var code_html = document.createElement("pre");
+          code_html.className = "js-code-example";
+          var line_number = parseInt(
+              el.getAttribute("data-code-example").split("#")[1]);
+          code_html.innerHTML = this.responseText.split(
+              "\n").slice(line_number, line_number + 25).join("\n");
+          el.insertBefore(code_html, el.firstChild);
+        }
+      }
+    }(images[i]);
+    request.send()
+  })();
+}
