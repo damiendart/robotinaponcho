6d75e0e857ea0ada836d449568f6142853f471ca
Damien Dart <damiendart@pobox.com>
2020-05-22 21:56:03 +0100

    Add ESLint gubbins and fix a code style issue.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,3 @@
-// Copyright (C) 2020 Damien Dart, <damiendart@pobox.com>.
-// This file is distributed under the MIT licence. For more information,
-// please refer to the accompanying "LICENCE" file.
-
 module.exports = {
   env: {
     browser: true,
