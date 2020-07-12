52f790a1ee84851985689eaff49bbc412f886f55
Damien Dart <damiendart@pobox.com>
2020-07-12 19:34:00 +0100

    Update "toolbox-sass" dependency.

--- static_src/.buildstatic.conf.js
+++ static_src/.buildstatic.conf.js
@@ -5,27 +5,29 @@
 const childProcess = require('child_process');
 
 module.exports = {
-  contextModifier: (context) => {
-    childProcess.exec(
-      `git log -n 1 --pretty=format:'%H %at' ${context.inputFile.name}`,
-      (error, stdout) => {
-        if (error) {
-          throw error;
+  itemsModifier: (items) => {
+    return items.map((item) => {
+      childProcess.exec(
+        `git log -n 1 --pretty=format:'%H %at' ${item.data.inputFilePath}`,
+        (error, stdout) => {
+          if (error) {
+            throw error;
+          }
+
+          const [hash, timestamp] = stdout.split(' ');
+
+          item.data.git = {
+            hash,
+            timestamp: new Date(parseInt(timestamp, 10)),
+          };
         }
+      );
 
-        const [hash, timestamp] = stdout.split(' ');
+      item.data.deployment = {
+        releaseTimestamp: process.env.RELEASE_TIMESTAMP || '00000000000000',
+      };
 
-        context.inputFile.git = {
-          hash,
-          timestamp: new Date(parseInt(timestamp, 10)),
-        };
-      }
-    );
-
-    context.deployment = {
-      releaseTimestamp: process.env.RELEASE_TIMESTAMP || '00000000000000',
-    };
-
-    return context;
+      return item;
+    });
   }
 };
