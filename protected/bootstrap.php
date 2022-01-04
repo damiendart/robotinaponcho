2a65f765b733722b19fd815d2e643f3c58004170
Damien Dart <damiendart@pobox.com>
2022-01-04 20:20:40 +0000

    Update copyright bumf.

--- protected/bootstrap.php
+++ protected/bootstrap.php
@@ -1,6 +1,6 @@
 <?php
 
-// Copyright (C) 2021 Damien Dart, <damiendart@pobox.com>.
+// Copyright (C) 2022 Damien Dart, <damiendart@pobox.com>.
 // This file is distributed under the MIT licence. For more information,
 // please refer to the accompanying "LICENCE" file.
 
