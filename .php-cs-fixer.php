6c5de8f240be5cc92e305e3cc5b47ba343fd22be
Damien Dart <damiendart@pobox.com>
2024-07-02 22:53:15 +0100

    Update ".php-cs-fixer.php".

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -1,12 +1,15 @@
 <?php
 
 /*
- * Copyright (C) 2023 Damien Dart, <damiendart@pobox.com>.
+ * Copyright (C) Damien Dart, <damiendart@pobox.com>.
  * This file is distributed under the MIT licence. For more information,
  * please refer to the accompanying "LICENCE" file.
  */
 
 declare(strict_types=1);
+
+use PhpCsFixer\Config;
+use PhpCsFixer\Finder;
 
 $header = <<<'HEADER'
 Copyright (C) Damien Dart, <damiendart@pobox.com>.
@@ -14,7 +17,7 @@ This file is distributed under the MIT licence. For more information,
 please refer to the accompanying "LICENCE" file.
 HEADER;
 
-return (new PhpCsFixer\Config())
+return (new Config())
     ->setRules(
         [
             '@PhpCsFixer' => true,
@@ -44,12 +47,13 @@ return (new PhpCsFixer\Config())
         ],
     )
     ->setFinder(
-        PhpCsFixer\Finder::create()
+        Finder::create()
+            ->ignoreDotFiles(false)
             ->notPath('node_modules')
             ->notPath('protected' . DIRECTORY_SEPARATOR . 'vendor')
             ->notPath('public')
             ->in(__DIR__)
             ->name('*.php')
             ->name(basename(__FILE__))
-            ->name('update-git-repositories')
+            ->name('update-git-repositories'),
     );
